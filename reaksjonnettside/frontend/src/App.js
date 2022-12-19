0a7f39736f809bdd32790c5d7cccd43ad791730c
Haidar Nuri <haidarnuri@gmail.com>
2022-12-19 10:56:43 +0100

    Created two pages
    Created Login and Reaction Page

--- reaksjonnettside/frontend/src/App.js
+++ reaksjonnettside/frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Mainpage from "./pages/Reactionsite/Mainpage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/reactionsite" element={<Mainpage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
